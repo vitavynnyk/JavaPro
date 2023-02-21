a71ab93328783171e49c9b7ddf3c1bcfab8fdb4b
Vynnyk Vita <vitavynnyk1995@gmail.com>
2023-02-21 15:36:22 +0200

    Homework7

---
+++ src/Main/Java/com/hillel/vynnyk/homeworks/homework7/FileData.java
@@ -0,0 +1,41 @@
+package com.hillel.vynnyk.homeworks.homework7;
+
+import java.util.Objects;
+
+public class FileData {
+
+    private String name;
+    private int size;
+
+
+    public FileData(String name,int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return  name + size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FileData fileData = (FileData) o;
+        return size == fileData.size && Objects.equals(name, fileData.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, size);
+    }
+}
