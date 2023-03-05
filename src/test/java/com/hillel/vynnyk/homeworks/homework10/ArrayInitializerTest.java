b84793cb4f00fdbb3e16f7d7a3ceb853859ed35b
Vynnyk Vita <vitavynnyk1995@gmail.com>
2023-03-05 17:41:05 +0200

    Corrected

---
+++ src/test/java/com/hillel/vynnyk/homeworks/homework10/ArrayInitializerTest.java
@@ -0,0 +1,22 @@
+package com.hillel.vynnyk.homeworks.homework10;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArrayInitializerTest {
+
+    @Test
+
+    public void shouldInitArray() {
+        double[] array = new double[12];
+        Arrays.fill(array,5);
+        ArrayInitializer.init(array);
+
+       for (int i = 0; i < array.length / 2; i++) {
+            assertEquals(array[i], array[array.length / 2 + i]);
+        };
+    }
+}
