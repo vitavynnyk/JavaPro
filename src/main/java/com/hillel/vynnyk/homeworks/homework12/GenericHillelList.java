7d7af294c4ae399c10373980e0a2bbf3c4c4edec
Vynnyk Vita <vitavynnyk1995@gmail.com>
2023-03-12 16:57:29 +0200

    Homework 12 Generics

---
+++ src/main/java/com/hillel/vynnyk/homeworks/homework12/GenericHillelList.java
@@ -0,0 +1,44 @@
+package com.hillel.vynnyk.homeworks.homework12;
+
+public interface GenericHillelList<T> {
+    /**
+     * Додати новий елемент до списка
+     */
+    void add(T item);
+
+    /**
+     * Видалити елемент із списка за визначенним індексом
+     *
+     * @return видалений елемент
+     */
+     T remove(int index);
+    /**
+     * Перевіряє чи є такий елемент у списку.
+     *
+     * @return true - якщо є, false - якщо ні
+     */
+     boolean contains(T item);
+
+    /**
+     * Знайти індекс елемента у списку
+     *
+     * @return індекс (0 - відповідає першому елементу), -1 - якщо не знайдено
+     */
+     int indexOf(T item);
+
+    /**
+     * Розмір списка
+     */
+    int size();
+
+    /**
+     * Повертає елемент списка за вказанним індексом
+     */
+     T get(int index);
+
+    /**
+     * Всі елементи списка
+     */
+    Object[] getAll();
+}
+
